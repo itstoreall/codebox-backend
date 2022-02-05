ed77a8a650524422c524288e2fc0a032598a9459
Serhii Stanislav <serhiistanislav@gmail.com>
2022-02-05 18:56:23 +0200

    adds some changes

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -3,10 +3,18 @@ import { Sequelize } from 'sequelize';
 import view from './View.js';
 import link from './Link.js';
 
-const sequelize = new Sequelize('codebox_db', '', '', {
-  dialect: 'postgres',
-  host: 'https://codebox-backend.herokuapp.com',
-});
+// const sequelize = new Sequelize('codebox_db', '', '', {
+const sequelize = new Sequelize(
+  'dav5hg1q7jg6u0',
+  'uakkhcinwpbwck',
+  '9903c2da837ece2fb1ba7af0094e5e2b78976d9883ae3189e57165dd232dc7f3',
+  {
+    dialect: 'postgres',
+    host: 'https://codebox-backend.herokuapp.com/graphql',
+    connection: process.env.DATABASE_URL,
+    // host: process.env.POSTGRES_HOST,
+  }
+);
 
 const View = view(sequelize);
 const Link = link(sequelize);
